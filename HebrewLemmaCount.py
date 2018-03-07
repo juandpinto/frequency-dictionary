958ac48eb83935aa56e5151c7d06049f5ae6e0f7
Juan D. Pinto <jdpinto1@gmail.com>
2018-03-07 13:44:04 -0600

    Added ability to count range (number of movies each word appears in), which is also included in the output CSV.

--- HebrewLemmaCount.py
+++ HebrewLemmaCount.py
@@ -3,7 +3,10 @@
 import re
 from pathlib import Path
 
-frequency = {}
+# Initialize frequency and range dictionaries
+frequency_dict = {}
+range_dict = {}
+range_bool = {}
 
 
 # --- FUNCTIONS ---
@@ -20,20 +23,27 @@ def open_and_read(file_loc):
 def find_and_count(doc):
     match_pattern = re.findall(r'lemma="[א-ת]+', doc)
     for word in match_pattern:
-        count = frequency.get(word[7:], 0)
-        frequency[word[7:]] = count + 1
+        count = frequency_dict.get(word[7:], 0)
+        frequency_dict[word[7:]] = count + 1
+        # print(word[7:])
+        range_bool[word[7:]] = True
+    for k in range_bool:
+        if range_bool[k]:
+            count = range_dict.get(k, 0)
+            range_dict[k] = count + 1
+            range_bool[k] = False
 
 
 # Sort entries in "frequency{}" dictionary by count.
 def sort_frequencies():
-    return [(k, frequency[k]) for k in sorted(
-        frequency, key=frequency.get, reverse=True)]
+    return [(k, frequency_dict[k]) for k in sorted(
+        frequency_dict, key=frequency_dict.__getitem__, reverse=True)]
 
 
 # -- MAIN CODE ---
 
 # Define path for topmost directory to search.
-p = Path('../OpenSubtitles2018_parsed/parsed/he/0')
+p = Path('../OpenSubtitles2018_parsed/parsed/he')
 p = list(p.glob('**/*.xml'))
 
 # Run "open_and_read()" and "find_and_count()" functions
@@ -47,5 +57,6 @@ frequency_sorted = sort_frequencies()
 # Write final tallies to CSV file.
 result = open('HebrewLemmaCount.csv', 'w')
 for k, v in frequency_sorted:
-    result.write(str(v) + ', ' + k + '\n')
+    result.write(str(v) + ', ' + k + ', ' + str(
+        range_dict[k]) + '\n')
 result.close()
