9008399b23955a633af43f2257d803d9bf32218c
Juan D. Pinto <jdpinto1@gmail.com>
2018-03-19 17:01:07 -0500

    Began creation of table and cleaned up code

--- HebrewLemmaCount.py
+++ HebrewLemmaCount.py
@@ -4,10 +4,13 @@
 import re
 from pathlib import Path
 
-# Initialize dictionary
+# Initialize dictionaries
 lemma_by_corpus_dict = {}
 lemma_totals_dict = {}
 token_count_dict = {}
+
+total_tokens = 0
+table_list = []
 
 
 # ----- FUNCTIONS -----
@@ -33,34 +36,11 @@ def find_and_count(doc):
             lemma_by_corpus_dict[word[7:-1]][corpus] = 1
 
 
-# Add total tokens per corpus
-def add_tokens():
-    for lemma in lemma_by_corpus_dict:
-        for corpus in lemma_by_corpus_dict[lemma]:
-            token_count_dict[corpus] = token_count_dict.get(
-                corpus, 0) + lemma_by_corpus_dict[lemma][corpus]
-
-
-# Add total frequencies per lemma
-def calculate_total_frequencies():
-    for lemma in lemma_by_corpus_dict:
-        for corpus in lemma_by_corpus_dict[lemma]:
-            lemma_totals_dict[lemma] = lemma_totals_dict.get(
-                lemma, 0) + lemma_by_corpus_dict[lemma][corpus]
-
-
-# Sort entries in "frequency{}" dictionary by count.
-def sort_frequencies():
-    return [(k, lemma_totals_dict[k]) for k in sorted(
-        lemma_totals_dict, key=lemma_totals_dict.__getitem__,
-            reverse=True)]
-
-
 # ----- MAIN CODE -----
 
 
 # Define path for topmost directory to search.
-p = Path('../OpenSubtitles2018_parsed/parsed/he/0/374995')
+p = Path('../OpenSubtitles2018_parsed/parsed/he/0')
 p = list(p.glob('**/*.xml'))
 
 # Run "open_and_read()" and "find_and_count()" functions
@@ -68,25 +48,42 @@ p = list(p.glob('**/*.xml'))
 for f in p:
     find_and_count(open_and_read(f))
 
-# Run "add_tokens()" and "calculate_total_frequencies()"
-add_tokens()
-calculate_total_frequencies()
+# Add total tokens per corpus for each lemma
+for lemma in lemma_by_corpus_dict:
+    for corpus in lemma_by_corpus_dict[lemma]:
+        token_count_dict[corpus] = token_count_dict.get(
+            corpus, 0) + lemma_by_corpus_dict[lemma][corpus]
 
-# Debugger:
-# for item in lemma_totals_dict:
-#     print(item + '  -  ' + str(lemma_totals_dict[item]))
+# Add total frequencies per lemma
+for lemma in lemma_by_corpus_dict:
+    lemma_totals_dict[lemma] = sum(lemma_by_corpus_dict[lemma].values())
 
-# Debugger:
-# for lemma in lemma_by_corpus_dict:
-#     for corpus in lemma_by_corpus_dict[lemma]:
-#         print(lemma + '  -  ' + corpus + '  -  ' + str(
-#             lemma_by_corpus_dict[lemma][corpus]))
+# Add total token counts per corpus
+for corpus in token_count_dict:
+    total_tokens = total_tokens + token_count_dict.get(corpus, 0)
 
-# Run "sort_frequencies()" function.
-frequency_sorted = sort_frequencies()
+# Sort entries in "frequency{}" dictionary by count.
+frequency_sorted_list = [(k, lemma_totals_dict[k]) for k in sorted(
+    lemma_totals_dict, key=lemma_totals_dict.__getitem__,
+    reverse=True)]
+
 
 # Write final tallies to CSV file.
-result = open('HebrewLemmaCount.csv', 'w')
-for k, v in frequency_sorted:
-    result.write(str(v) + ', ' + k + ', ' + '\n')
-result.close()
+# result = open('HebrewLemmaCount.csv', 'w')
+# for k, v in frequency_sorted_list:
+#     result.write(str(v) + ', ' + k + ', ' + '\n')
+# result.close()
+
+# Create list of tuples with all values
+# table_list = [(Lemma, Frequency, Range, DP)]
+# table_list = [(k, v, sum(lemma_by_corpus_dict[k].values()))
+#     for k, v in frequency_sorted_list[:20]]
+for k, v in frequency_sorted_list[:20]:
+    table_list.append((k, v, sum(
+        1 for count in lemma_by_corpus_dict[k].values() if count > 0)))
+
+# print(table_list)
+for i in range(20):
+    print('Lemma: ' + table_list[i][0] +
+          '\tFrequency: ' + str(table_list[i][1]) +
+          '\t\tRange: ' + str(table_list[i][2]))
