164f39da005c8781097a0a0213d9821f87a06b91
Juan D. Pinto <jdpinto1@gmail.com>
2018-03-20 11:59:54 -0500

    Added calculation and export for UDP, cleaned up code

--- HebrewLemmaCount.py
+++ HebrewLemmaCount.py
@@ -3,14 +3,20 @@
 
 import re
 from pathlib import Path
+from collections import defaultdict
 
 # Initialize dictionaries
 lemma_by_corpus_dict = {}
 lemma_totals_dict = {}
 token_count_dict = {}
+lemma_DPs_dict = defaultdict(float)
+lemma_UDPs_dict = defaultdict(float)
 
-total_tokens = 0
+total_tokens_int = 0
 table_list = []
+
+# Identify size of list
+list_size_int = 5000
 
 
 # ----- FUNCTIONS -----
@@ -39,8 +45,8 @@ def find_and_count(doc):
 # ----- MAIN CODE -----
 
 
-# Define path for topmost directory to search.
-p = Path('../OpenSubtitles2018_parsed/parsed/he/0')
+# Define path for topmost directory to search. (0/374995)
+p = Path('../OpenSubtitles2018_parsed/parsed/he')
 p = list(p.glob('**/*.xml'))
 
 # Run "open_and_read()" and "find_and_count()" functions
@@ -48,42 +54,66 @@ p = list(p.glob('**/*.xml'))
 for f in p:
     find_and_count(open_and_read(f))
 
-# Add total tokens per corpus for each lemma
+# Calculate token count per corpus
 for lemma in lemma_by_corpus_dict:
     for corpus in lemma_by_corpus_dict[lemma]:
         token_count_dict[corpus] = token_count_dict.get(
             corpus, 0) + lemma_by_corpus_dict[lemma][corpus]
 
-# Add total frequencies per lemma
+# Calculate total frequencies per lemma
 for lemma in lemma_by_corpus_dict:
     lemma_totals_dict[lemma] = sum(lemma_by_corpus_dict[lemma].values())
 
-# Add total token counts per corpus
+# Calculate total token count
 for corpus in token_count_dict:
-    total_tokens = total_tokens + token_count_dict.get(corpus, 0)
+    total_tokens_int = total_tokens_int + token_count_dict.get(corpus, 0)
 
 # Sort entries in "frequency{}" dictionary by count.
 frequency_sorted_list = [(k, lemma_totals_dict[k]) for k in sorted(
     lemma_totals_dict, key=lemma_totals_dict.__getitem__,
     reverse=True)]
 
+# Calculate DPs
+for lemma in lemma_by_corpus_dict.keys():
+    for corpus in lemma_by_corpus_dict[lemma].keys():
+        lemma_DPs_dict[lemma] = lemma_DPs_dict[lemma] + abs(
+            (token_count_dict[corpus] /
+             total_tokens_int) -
+            (lemma_by_corpus_dict[lemma][corpus] /
+             lemma_totals_dict[lemma]))
+lemma_DPs_dict = {lemma: DP/2 for (lemma, DP) in lemma_DPs_dict.items()}
 
-# Write final tallies to CSV file.
-# result = open('HebrewLemmaCount.csv', 'w')
+# Calculate UDPs
+lemma_UDPs_dict = {lemma: 1-DP for (lemma, DP) in lemma_DPs_dict.items()}
+
+
+# Create list of tuples with all values
+# Lemma, Frequency, Range, DP
+# table_list = [(k, v, sum(lemma_by_corpus_dict[k].values()))
+#     for k, v in frequency_sorted_list[:20]]
+for k, v in frequency_sorted_list[:list_size_int]:
+    table_list.append((k, v, sum(
+        1 for count in lemma_by_corpus_dict[k].values() if count > 0),
+        lemma_UDPs_dict[k]))
+
+
+# print(table_list)
+# for i in range(list_size_int):
+#     print('Lemma: ' + table_list[i][0] +
+#           '\tFrequency: ' + str(table_list[i][1]) +
+#           '\tRange: ' + str(table_list[i][2]) +
+#           '\tUDP: ' + str(table_list[i][3]))
+
+
+# Write final tallies to CSV file
+result = open('HebrewLemmaCount.csv', 'w')
 # for k, v in frequency_sorted_list:
 #     result.write(str(v) + ', ' + k + ', ' + '\n')
 # result.close()
-
-# Create list of tuples with all values
-# table_list = [(Lemma, Frequency, Range, DP)]
-# table_list = [(k, v, sum(lemma_by_corpus_dict[k].values()))
-#     for k, v in frequency_sorted_list[:20]]
-for k, v in frequency_sorted_list[:20]:
-    table_list.append((k, v, sum(
-        1 for count in lemma_by_corpus_dict[k].values() if count > 0)))
-
-# print(table_list)
-for i in range(20):
-    print('Lemma: ' + table_list[i][0] +
-          '\tFrequency: ' + str(table_list[i][1]) +
-          '\t\tRange: ' + str(table_list[i][2]))
+result.write('LEMMA, FREQUENCY, RANGE, UDP\n')
+for i in range(list_size_int):
+    result.write(str(table_list[i][0]) + ', ' +
+                 str(table_list[i][1]) + ', ' +
+                 str(table_list[i][2]) + ', ' +
+                 str(table_list[i][3]) + '\n')
+result.close()
